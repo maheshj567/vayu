a94717459bc4bee1cb29867032c08744e4fa74e6
Mahesh J <mahesh.j567@gmail.com>
2012-10-25 14:43:41 +0800

    first commit
    A simple to-do's app, as an experiment involving HTML5 local storage

---
+++ javascripts/data.js
@@ -0,0 +1,27 @@
+/**
+ * @author Mahesh
+ */
+
+// var boards = [
+	// {id: "1", name: "Office", cards: ["1", "2"]},
+	// {id: "2", name: "Home", cards: ["3", "4"]},
+	// {id: "3", name: "Getoo"}
+// ];
+// 
+// var cards = [
+	// {id: "1", name: "Morning", dodos: ["1", "2"]},
+	// {id: "2", name: "Afternoon", dodos: ["3", "4"]},
+	// {id: "3", name: "Groceries", dodos: ["5", "6"]},
+	// {id: "4", name: "BITSAA", dodos: ["7", "8"]}
+// ];
+// 
+// var dodos = [
+	// {id: "1", title: "Facebook", done: false},
+	// {id: "2", title: "ops work", done: false},
+	// {id: "3", title: "Dev work", done: false},
+	// {id: "4", title: "Finish GSEC excel downloads stuff", done: false},
+	// {id: "5", title: "Oranges", done: false},
+	// {id: "6", title: "Bread", done: false},
+	// {id: "7", title: "Understand SSO", done: false},
+	// {id: "8", title: "Implement SSO", done: false}
+// ];
