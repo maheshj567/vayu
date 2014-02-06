2cfaa4de0991ddb346136bec422bf1badf3fc4cc
Mahesh J <mahesh.j567@gmail.com>
2014-02-06 22:29:46 +0530

    FB login with mongodb support for user storage

--- server.js
+++ server.js
@@ -3,32 +3,61 @@
  */
 
 var express = require('express'),
-    path = require('path');
+    hbs = require('hbs'),
+    path = require('path'),
+    passport = require('passport'),
+    vayu = require('vayu');
 
 var app = module.exports = express();
 
 app.configure(function () {
     app.set('port', process.env.port || 3000);
+    
     app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
+    app.set('view engine', 'html');
+    // app.register('html', require('hbs'));
+    app.engine('html', require('hbs').__express);
+    
     app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
+    app.use( express.cookieParser());
     app.use(express.methodOverride());
+    app.use(express.session({ secret: 'keyboard cat' }));
+    
+    app.use(passport.initialize());
+    app.use(passport.session());
+    
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
-});
+    
+    //handlebars extensions
+    var blocks = {};
+    hbs.registerHelper('extend', function (name, context) {
+        var block = blocks[name];
+        if (!block) {
+            block = blocks[name] = [];
+        }
 
-app.get("/hi", function(req, res){
-    var body = 'Hello World';
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Length', Buffer.byteLength(body));
-    res.end(body);
+        block.push(context.fn(this)); // for older versions of handlebars, use block.push(context(this));
+    });
+
+    hbs.registerHelper('block', function (name) {
+        var val = (blocks[name] || []).join('\n');
+
+        // clear the block
+        blocks[name] = [];
+        return val;
+    });
+    
+    hbs.registerPartials(__dirname + '/views/partials');
 });
 
 app.configure('development', function () {
     app.use(express.errorHandler());
 });
+
+vayu.init(app);
 
 app.listen(app.get('port'));
 console.log('Listening on port ' + app.get('port'));
