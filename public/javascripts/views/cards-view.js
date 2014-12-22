b4164ad0fc11243e46afde3bb98ee8454d7225a0
Mahesh J <mahesh.j567@gmail.com>
2014-12-22 08:55:40 +0530

    refactoring ideas for facade, boards-view, cards-view

--- public/javascripts/views/cards-view.js
+++ public/javascripts/views/cards-view.js
@@ -6,11 +6,13 @@ define(["views/card-item-view"], function(CardItemView)
 		cardViews : [],
 
 		initialize : function() {
+			// REF: pass cards_coll to the constructor
 			this.collection = window.cards_coll;
 
 			this.collection.on("reset", this.renderCards, this);
 			this.collection.on("add", this.addCard, this);
 
+			// ref: not a great place for this. CardsView should only manage cards 
 			window.vtodos_coll.on("reset", this.renderVtodos, this);
 
 			this.renderCards();
@@ -19,8 +21,10 @@ define(["views/card-item-view"], function(CardItemView)
 		renderCards : function() {
 			this.$el.find("div").remove();
 
+			// ref: cardViews shouldn't be needed
 			cardViews = [];
 
+			// ref: listManager
 			_.each(this.collection.models, function(item) {
 				var view = this.renderCardItem(item);
 				cardViews.push(view);
@@ -31,6 +35,7 @@ define(["views/card-item-view"], function(CardItemView)
 
 		addCard : function() {
 			this.renderCardItem(arguments[0], true).renderVtodos();
+			// refereshDimensions should be part of app logic
 			refreshDimensions();
 		},
 
@@ -51,14 +56,14 @@ define(["views/card-item-view"], function(CardItemView)
 		},
 
 		renderVtodos : function() {
+			// ref: this should be handled by vtodos list
 			for(var i=0; i < cardViews.length; i++)
 			{
 				cardViews[i].renderVtodos();
 			}
 
+			// ref: this should be in a more central location, may be as part of a handler for an event
 			window.refreshDimensions();
-
-			// $('.done-vtodo').css("display", "none");
 		}
 	});
 	
